b30b5c25097a0545b224b694a9771a2cdd425671
Matt Heitzenroder <mheitzenroder@gmail.com>
2011-09-05 22:14:17 -0400

    adds a hacky way of saving emails, #worksfornow

--- lib/capsulecrm/contact.rb
+++ lib/capsulecrm/contact.rb
@@ -1,12 +1,27 @@
 class CapsuleCRM::Contact < CapsuleCRM::Child
 
   attr_accessor :type
+  define_attribute_methods [:type]
 
+  # nodoc
+  def attributes
+    attrs = {}
+    arr = [:type]
+    arr.each do |key|
+      attrs[key] = self.send(key)
+    end
+    attrs
+  end
+  
+  # nodoc
+  def type=(value)
+    type_will_change! unless value == type
+    @type = value
+  end
+  
   # nodoc
   def self.xml_map
     map = {'type' => 'type'}
     super.merge map
   end
-
-
 end
--- lib/capsulecrm/email.rb
+++ lib/capsulecrm/email.rb
@@ -1,8 +1,57 @@
 class CapsuleCRM::Email < CapsuleCRM::Contact
 
   attr_accessor :address
+  define_attribute_methods [:address]
 
 
+  # nodoc
+  def attributes
+    attrs = {}
+    arr = [:address, :type]
+    arr.each do |key|
+      attrs[key] = self.send(key)
+    end
+    attrs
+  end
+  
+  # nodoc
+  def address=(value)
+    address_will_change! unless value == address
+    @address = value
+  end
+  
+  # nodoc
+  def dirty_attributes
+    Hash[attributes.select { |k,v| changed.include? k.to_s }]
+  end
+  
+  def parent_type
+    return "person" if self.parent.class == CapsuleCRM::Person
+    return "organisation" if self.parent.class == CapsuleCRM::Organisation
+    raise "Unknown Parent Type"
+  end
+  
+  # nodoc
+  def save
+    path = ["", "api", parent_type, self.parent.id].join("/")
+    options = {:root => 'person', :path => path}
+    attrs = new_record?? attributes : {:id => id}.merge(dirty_attributes)
+    success = self.class.update id, attrs, options
+    changed_attributes.clear if success
+    success   
+  end
+  
+  # uses xml_map() to convert :attributes into an xml string
+  def self.attributes_to_xml(attributes, root=nil)
+    xml = {"contacts" => {"email" => {}}}
+    map = xml_map.invert
+    attributes.each do |k,v|
+      key = map[k.to_s]
+      xml["contacts"]["email"][key] = v
+    end
+    xml.to_xml :root => root
+  end
+  
   # nodoc
   def self.xml_map
     map = {
